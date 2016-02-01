3b7748f950475d331d6f03cda09214bcbd34a7d8
buirai <buirai_naetle@hotmail.com>
2016-02-01 09:45:19 -0600

    Se agrego un poco de estilo al index y al formulario

--- app/models/scholarship.rb
+++ app/models/scholarship.rb
@@ -1,3 +1,12 @@
 class Scholarship < ActiveRecord::Base
   belongs_to :university
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :university, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :description, presence: true
+  validates :requirements, presence: true
+  validates :benefits_offered, presence: true
+  validates :url, presence: true
 end
