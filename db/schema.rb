894bc25e9cc3e5ce639a2c5d12a718b7c39ea094
Lalo_Canche <eduardo_benji@hotmail.com>
2016-02-02 20:21:35 -0600

    Se cambia el database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-# ActiveRecord::Schema.define(version: 20160201142954) do
-
 ActiveRecord::Schema.define(version: 20160201181747) do
 
   create_table "person", force: :cascade do |t|
