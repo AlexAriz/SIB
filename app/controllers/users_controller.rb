7ad1b4b150f609177ae08fe4a2755eaab946fe2d
Lalo_Canche <eduardo_benji@hotmail.com>
2016-02-02 20:17:23 -0600

    Cambios de acuerdo a rubocop.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# The controller for showing and deleting
+# the users registered in the system.
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :destroy]
   before_action :authenticate_user!
@@ -11,12 +13,15 @@ class UsersController < ApplicationController
 
   def destroy
     if current_user.id == @user.id
-      redirect_to :index, notice: 'No se pudo eliminar el usuario, ya que es usted mismo'
+      redirect_to :index, notice: 'No se pudo eliminar el usuario,
+                                  ya que es usted mismo'
     else
       @user.destroy
-      redirect_to users_index_url, notice: 'Se ha eliminado exitosamente el usuario'
+      redirect_to users_index_url, notice: 'Se ha eliminado exitosamente el
+                                            usuario'
     end
   end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
