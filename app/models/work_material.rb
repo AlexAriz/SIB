fe72860b408a1f9031bc7b134aec0bafc38fe711
AlexAriz <alexariztegui@zoho.com>
2016-02-06 12:40:14 -0600

    Crear material de trabajo con las relaciones correctas a los tipos de usuario

--- app/models/work_material.rb
+++ app/models/work_material.rb
@@ -1,5 +1,7 @@
 class WorkMaterial < ActiveRecord::Base
   belongs_to :tutor, :class_name => 'User'
   has_and_belongs_to_many :candidates, :class_name => 'User'
-
+  validates :name, presence: true, length: {minimum: 8}
+  validates :description, presence: true
+  validates :file_src, presence: true
 end
