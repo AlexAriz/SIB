9cddbae0af2fd887fe43381cbc2ebf33ebcc4429
buirai <buirai_naetle@hotmail.com>
2016-02-08 23:01:06 -0600

    Se ha modificado el modelo de la Beca

--- app/models/scholarship.rb
+++ app/models/scholarship.rb
@@ -6,8 +6,8 @@ class Scholarship < ActiveRecord::Base
   validates :university, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :description, presence: true
-  validates :requirements, presence: true
-  validates :benefits_offered, presence: true
+  validates :description, presence: true, length: { minimum: 50, maximum: 400 }
+  validates :requirements, presence: true, length: { minimum: 10, maximum: 400 }
+  validates :benefits_offered, presence: true, length: { minimum: 10, maximum: 400 }
   validates :url, presence: true
 end
