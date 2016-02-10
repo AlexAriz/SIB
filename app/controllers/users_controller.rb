0be657a07438be02c20ee423206907c52d499f0d
Lalo_Canche <eduardo_benji@hotmail.com>
2016-02-09 22:55:57 -0600

    Se terminan las funcionalidades del CU-18 por fin! falta cancancan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 # The controller for showing and deleting
 # the users registered in the system.
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :destroy]
+  before_action :set_user, only: [:show, :destroy, :edit, :update]
   before_action :authenticate_user!
   before_filter :check_for_database
 
   def index
     @users = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_show_path(@user),
+                                  notice: 'Haz editado tu perfil de manera exitosa' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def show
@@ -28,6 +42,48 @@ class UsersController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    if params[:admin].present?
+      params.require(:admin).permit(:email,
+                                    :user_name,
+                                    :image_profile,
+                                    person_attributes: [:id,
+                                                        :name,
+                                                        :last_name,
+                                                        :university,
+                                                        :area_of_interest,
+                                                        :comments,
+                                                        :first_choice,
+                                                        :_destroy])
+    elsif params[:tutor].present?
+      params.require(:tutor).permit(:email,
+                                    :user_name,
+                                    :image_profile,
+                                    person_attributes: [:id,
+                                                        :name,
+                                                        :last_name,
+                                                        :university,
+                                                        :area_of_interest,
+                                                        :comments,
+                                                        :first_choice,
+                                                        :_destroy])
+    else
+      params[:candidate].present?
+      params.require(:candidate).permit(:email,
+                                        :user_name,
+                                        :image_profile,
+                                        person_attributes: [:id,
+                                                            :name,
+                                                            :last_name,
+                                                            :university,
+                                                            :area_of_interest,
+                                                            :comments,
+                                                            :first_choice,
+                                                            :_destroy])
+    end
+
   end
 
   def check_for_database
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210002443) do
+ActiveRecord::Schema.define(version: 20160210043611) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name",             limit: 255
@@ -26,16 +26,6 @@ ActiveRecord::Schema.define(version: 20160210002443) do
   end
 
   add_index "people", ["user_id"], name: "index_people_on_user_id", using: :btree
-
-  create_table "person", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "last_name",  limit: 255
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  add_index "person", ["user_id"], name: "index_person_on_user_id", using: :btree
 
   create_table "scholarships", force: :cascade do |t|
     t.string   "name",             limit: 255
@@ -64,25 +54,29 @@ ActiveRecord::Schema.define(version: 20160210002443) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "type",                   limit: 255
-    t.string   "user_name",              limit: 255
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                      limit: 255, default: "", null: false
+    t.string   "encrypted_password",         limit: 255, default: "", null: false
+    t.string   "type",                       limit: 255
+    t.string   "user_name",                  limit: 255
+    t.string   "reset_password_token",       limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",              limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
+    t.string   "current_sign_in_ip",         limit: 255
+    t.string   "last_sign_in_ip",            limit: 255
+    t.string   "confirmation_token",         limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.integer  "tutor_id",               limit: 4
+    t.string   "unconfirmed_email",          limit: 255
+    t.datetime "created_at",                                          null: false
+    t.datetime "updated_at",                                          null: false
+    t.integer  "tutor_id",                   limit: 4
+    t.string   "image_profile_file_name",    limit: 255
+    t.string   "image_profile_content_type", limit: 255
+    t.integer  "image_profile_file_size",    limit: 4
+    t.datetime "image_profile_updated_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
@@ -109,7 +103,6 @@ ActiveRecord::Schema.define(version: 20160210002443) do
   end
 
   add_foreign_key "people", "users"
-  add_foreign_key "person", "users"
   add_foreign_key "scholarships", "universities"
   add_foreign_key "users_work_materials", "work_materials"
 end
