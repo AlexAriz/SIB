69d48c46f100721b9c38459a09bb09aede837815
AlexAriz <alexariztegui@zoho.com>
2016-02-10 10:14:15 -0600

    Cambio de nombre

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.action_mailer.default_url_options = {host: 'https://sistemaintegralbecas.herokuapp.com/'}
+  config.action_mailer.default_url_options = {host: 'https://sistemabecas.herokuapp.com/'}
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
