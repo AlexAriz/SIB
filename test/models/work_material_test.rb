07a278ebd3d3a9ea1c4bf92a1c9b2b4e7ad63e5e
Lalo_Canche <eduardo_benji@hotmail.com>
2016-02-19 12:00:38 -0600

    Se termina el CU-05.

--- test/models/work_material_test.rb
+++ test/models/work_material_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: work_materials
+#
+#  id                      :integer          not null, primary key
+#  name                    :string(255)
+#  description             :text(65535)
+#  tutor_id                :integer
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  attachment_file_name    :string(255)
+#  attachment_content_type :string(255)
+#  attachment_file_size    :integer
+#  attachment_updated_at   :datetime
+#
+
 require 'test_helper'
 
 class WorkMaterialTest < ActiveSupport::TestCase
