4bcde4082371d898cc3c240adaaefc7123ca6de4
AlexAriz <alexariztegui@zoho.com>
2016-02-22 00:00:40 -0600

    Plantillas para el mailer de progreso de material de trabajo

---
+++ app/mailers/users_work_material_mailer.rb
@@ -0,0 +1,9 @@
+class UsersWorkMaterialMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def update_progress(users_work_material)
+    @users_work_material = users_work_material
+    mail(to: @users_work_material.tutor.email,
+         subject: 'Actualización de un material de trabajo')
+  end
+end
