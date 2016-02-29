513cda39050887007c4d5df79c49f6fb4dd6272f
AlexAriz <alexariztegui@zoho.com>
2016-02-29 17:13:45 -0600

    Hotfix de tablas de usuarios

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require dataTables/extras/dataTables.responsive
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-//= require dataTables/extras/dataTables.tableTools
 //= require data-confirm-modal
 //= require bootstrap-select
 //= require is_js_rails
